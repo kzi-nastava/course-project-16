85c4052d4a27119edb88a07064d79f3daffb3c27
Veljko Bubnjevic <veljkobubnjevic01@gmail.com>
2022-04-28 15:01:31 +0200

    Factory and managers

---
+++ Usi_Projekat/Manage/Factory.cs
@@ -0,0 +1,30 @@
+﻿using Usi_Projekat.Manage;
+using Usi_Projekat.Settings;
+namespace Usi_Projekat.Manage
+{
+    public class Factory
+    {
+        private FileSettings _fileSettings;
+        private DirectorManager _directorManager;
+        private PatientManager _patientManager;
+        private SecretaryManager _secretaryManager;
+        private DoctorManager _doctorManager;
+
+        public Factory(FileSettings fileSettings)
+        {
+            _fileSettings = fileSettings;
+            _directorManager = new DirectorManager(_fileSettings.DirectorFilename, this);
+            _patientManager = new PatientManager(_fileSettings.PatientFilename, this);
+            _secretaryManager = new SecretaryManager(_fileSettings.SecretaryFilename, this);
+            _doctorManager = new DoctorManager(_fileSettings.DoctorFilename, this);
+        }
+        public void loadData()
+        {
+            _directorManager.loadData();
+            _patientManager.loadData();
+            _secretaryManager.loadData();
+            _doctorManager.loadData();
+        }
+        
+    }
+}
--- Usi_Projekat/Program.cs
+++ Usi_Projekat/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
-
-using Usi_Projekat.Methods;
-using Usi_Projekat.Users;
+using Usi_Projekat.IOController;
+using Usi_Projekat.Manage;
+using Usi_Projekat.Settings;
 
 namespace Usi_Projekat
 {
@@ -9,85 +9,93 @@ namespace Usi_Projekat
     {
         static void Main(string[] args)
         {
-           IOController.Factory  factory = new IOController.Factory();
-           Utills utills = new Utills(factory);
-           utills.readUsers("./../../../users.json", "", "", "");
-
-            while (true)
-            {
-                var ind = 0;
-                Console.WriteLine("Enter role:");
-                Console.WriteLine("1) - Director");
-                Console.WriteLine("2) - Doctor");
-                Console.WriteLine("3) - Patient");
-                Console.WriteLine("4) - Secretary");
-                Console.WriteLine("Enter email:");
-                string username = Console.ReadLine();
-                Console.WriteLine("Enter password:");
-                string password = Console.ReadLine();
-                string role = Console.ReadLine();
-                switch (role)
-                {
-                    case "1":
-                    {
-                        if (username == factory.director.email && password == factory.director.password)
-                        {
-                            Message();
-                            // factory.director.printMenu();
-                            break;
-                        }
-                    }
-                        break;
-
-                    case "2":
-                        foreach (var doctor in factory.doctors)
-                        {
-                            if (username == doctor.email && password == doctor.password)
-                            {
-                                Message();
-                                // doctor.printMenu();
-                                break;
-                            }
-                        }
-
-                        break;
-
-                    case "3":
-                        foreach (var patient in factory.patients)
-                        {
-                            if (username == patient.email && password == patient.password && patient.Blocked == 0)
-                            {
-                                Message();
-                                // patient.PrintMenu();
-                                Console.WriteLine(patient.name);
-                                break;
-                            }
-                        }
-
-                        break;
-
-                    case "4":
-                        foreach (var secretary in factory.secretaries)
-                        {
-                            if (username == secretary.email && password == secretary.password)
-                            {
-                                Message();
-                                //  secretary.PrintMenu();
-                                break;
-                            }
-                        }
-
-                        break;
-                    default:
-                        Console.WriteLine("Invalid Input, try again:");
-                        break;
-                }
-            }
-        }
-
-        public static void Message()
-        {
-            Console.WriteLine("Uspesno ste se ulogovali");
+            FileSettings fileSettings = new FileSettings("doctorFilename", "patientFilename",
+                "directorFilename", "secretaryFilename");
+            Factory factory = new Factory(fileSettings);
+            factory.loadData();
+            CheckInfo checkInfo = new CheckInfo(factory);
+            checkInfo.PrintMenu();
+            
+            //
+            //    Utills utills = new Utills(factory);
+            //    utills.readUsers("./../../../users.json", "", "", "");
+            //
+            //     while (true)
+            //     {
+            //         var ind = 0;
+            //         Console.WriteLine("Enter role:");
+            //         Console.WriteLine("1) - Director");
+            //         Console.WriteLine("2) - Doctor");
+            //         Console.WriteLine("3) - Patient");
+            //         Console.WriteLine("4) - Secretary");
+            //         Console.WriteLine("Enter email:");
+            //         string username = Console.ReadLine();
+            //         Console.WriteLine("Enter password:");
+            //         string password = Console.ReadLine();
+            //         string role = Console.ReadLine();
+            //         switch (role)
+            //         {
+            //             case "1":
+            //             {
+            //                 if (username == factory.director.email && password == factory.director.password)
+            //                 {
+            //                     Message();
+            //                     // factory.director.printMenu();
+            //                     break;
+            //                 }
+            //             }
+            //                 break;
+            //
+            //             case "2":
+            //                 foreach (var doctor in factory.doctors)
+            //                 {
+            //                     if (username == doctor.email && password == doctor.password)
+            //                     {
+            //                         Message();
+            //                         // doctor.printMenu();
+            //                         break;
+            //                     }
+            //                 }
+            //
+            //                 break;
+            //
+            //             case "3":
+            //                 foreach (var patient in factory.patients)
+            //                 {
+            //                     if (username == patient.email && password == patient.password && patient.Blocked == 0)
+            //                     {
+            //                         Message();
+            //                         // patient.PrintMenu();
+            //                         Console.WriteLine(patient.name);
+            //                         break;
+            //                     }
+            //                 }
+            //
+            //                 break;
+            //
+            //             case "4":
+            //                 foreach (var secretary in factory.secretaries)
+            //                 {
+            //                     if (username == secretary.email && password == secretary.password)
+            //                     {
+            //                         Message();
+            //                         //  secretary.PrintMenu();
+            //                         break;
+            //                     }
+            //                 }
+            //
+            //                 break;
+            //             default:
+            //                 Console.WriteLine("Invalid Input, try again:");
+            //                 break;
+            //         }
+            //     }
+            // }
+            //
+            // public static void Message()
+            // {
+            //     Console.WriteLine("Uspesno ste se ulogovali");
+            // }
         }
     }
 }
