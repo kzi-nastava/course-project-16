dcdc370a26f213ad0386a59bf16dd22cfb34b586
branislavstojkovic70 <96660978+branislavstojkovic70@users.noreply.github.com>
2022-05-14 19:05:03 +0200

    error with commit solved

--- Usi_Project/Recipes.cs
+++ Usi_Project/Recipes.cs
@@ -8,6 +8,36 @@ namespace Usi_Project
         public string timesADay;
         public string timeRelFood;
 
+        public string CureName
+        {
+            get => cureName;
+            set => cureName = value;
+        }
+
+        public string EmailPatient
+        {
+            get => emailPatient;
+            set => emailPatient = value;
+        }
+
+        public string TimeInstructions
+        {
+            get => timeInstructions;
+            set => timeInstructions = value;
+        }
+
+        public string TimesADay
+        {
+            get => timesADay;
+            set => timesADay = value;
+        }
+
+        public string TimeRelFood
+        {
+            get => timeRelFood;
+            set => timeRelFood = value;
+        }
+
         public Recipes(string cureName,string emailPatient, string timeInstructions, string timesADay, string timeRelFood)
         {
             this.cureName = cureName;
